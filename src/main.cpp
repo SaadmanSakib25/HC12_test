5359c887d2b088f267f6721687e879e99aec72cd
Saadman Sakib <62670617+SaadmanSakib25@users.noreply.github.com>
2022-02-26 10:55:27 +0600

    transceiver test

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,38 @@
 #include <Arduino.h>
 #include<SoftwareSerial.h>
-
+#include <EEPROM.h>
 
 #define DEBUG					true
 #define MAX_MSG_SIZE			128
 #define HC12_TX                 10
 #define HC12_RX                 11
 #define LED                     9
-#define PUSH_BUTTON             2
-#define DEBOUNCE_TIME             500
+#define LED2                    8
+#define PUSH_BUTTON_1           2
+#define PUSH_BUTTON_2           3
+#define DEBOUNCE_TIME           500
+#define NumOf_BAUDRATE          6
 
-
+uint32_t baudRates[NumOf_BAUDRATE] = {1200, 2400, 4800, 9600, 57600, 115200};
 
 bool hc12_msg_rcv = false;
 unsigned long tic;
 unsigned int counter = 0;
-bool button_pressed = 0;
+bool button1_pressed = 0;
+bool button2_pressed = 0;
 bool isr_started = false;
-
+// uint32_t elapsed = millis();
 SoftwareSerial HC12(HC12_TX, HC12_RX);
+
+void vSoftwareReset(unsigned long delayMillis) 
+{
+  uint32_t resetTime = millis() + delayMillis;
+  while ( resetTime > millis()) { 
+    /* wait and do nothing until the required delay expires... */
+  }
+  // jump to the start of the program
+  asm volatile ( "jmp 0");
+}
 
 
 void vHC12Send(const char* msg, SoftwareSerial &HC12)
@@ -60,12 +74,18 @@ void vHC12Receive(char* buffer, uint16_t buffer_size, SoftwareSerial &HC12)
 }
 
 
-void vPushButtonPressed()
+void vPushButton1Pressed()
 {
-    button_pressed = 1;
+    button1_pressed = 1;
 }
 
-void concat(char str1[], char str2[], char buff[])
+void vPushButton2Pressed()
+{
+    button2_pressed = 1;
+}
+
+
+void concat(const char str1[], const char str2[], char buff[])
 {
     int final_length = sizeof(str1) + sizeof(str2);
     // Serial.println(final_length);
@@ -87,26 +107,78 @@ void concat(char str1[], char str2[], char buff[])
     buff[i] = '\0';
 }
 
+void vBaudBlink ()
+{
+    int ind;
+    EEPROM.get(0, ind);
+    // if(millis()<5000) {
+        for (int x = 0; x <= ind; x++){
+        digitalWrite(LED2, HIGH);
+        delay(250);
+        digitalWrite(LED2, LOW);
+        delay(250);
+        }
+    // }
+}
+
 void setup() 
 {
     pinMode(LED, OUTPUT);
-    pinMode(PUSH_BUTTON, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(PUSH_BUTTON), vPushButtonPressed, RISING);
-	Serial.begin(9600);
-	HC12.begin(9600);
+    pinMode(LED2, OUTPUT);
+    pinMode(PUSH_BUTTON_1, INPUT_PULLUP);
+    pinMode(PUSH_BUTTON_2, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(PUSH_BUTTON_1), vPushButton1Pressed, RISING);
+    attachInterrupt(digitalPinToInterrupt(PUSH_BUTTON_2), vPushButton2Pressed, RISING);
+    //Serial.begin(DEFAULT_BAUDRATE);
+	//HC12.begin(DEFAULT_BAUDRATE); 
+    int incrm = 0;
+    int ind;
+    EEPROM.get(0, ind);
+    Serial.begin(baudRates[ind]);
+    HC12.begin(baudRates[ind]);
+    vBaudBlink ();
+    // for (int x = 0; x < ind; x++){
+    //     digitalWrite(LED2, HIGH);
+    //     delay(250);
+    //     digitalWrite(LED2, LOW);
+    //     delay(250);
+    // }
 
-	// delay(3000);
-	
     char msg[MAX_MSG_SIZE], rcv_msg[MAX_MSG_SIZE];
-    button_pressed = 0;
+    button1_pressed = 0;
+    button2_pressed = 0;
     // tic = millis();
 	while (1)
 	{
         // while (!bPushButtonPressed());
+        // EEPROM.get(0,ind);
+        // for (int x = 0; x < ind; x++){
+        //     digitalWrite(LED, HIGH);
+        //     delay(200);
+        //     digitalWrite(LED, LOW);
+        //     delay(200);
+        // }
 
-        if(button_pressed)
+        if(button2_pressed) {
+            button2_pressed = 0;
+            EEPROM.get(0,ind);
+            incrm = ind;
+            if (incrm<NumOf_BAUDRATE-1) {
+                incrm++;
+                EEPROM.put(0, incrm);
+
+            }
+            else if (incrm=>NumOf_BAUDRATE-1) {
+                incrm = 0;
+                EEPROM.put(0, incrm);
+            }
+            // Serial.println(F("Pressed"));
+            vSoftwareReset(100);
+        }
+
+        if(button1_pressed)
         {
-            button_pressed = 0;
+            button1_pressed = 0;
             counter++;
 
             char count[16];
